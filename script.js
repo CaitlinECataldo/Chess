69987625e644121e3fa0bcc1a94675a248ee5c16
CaitlinECataldo <caitlinreitz@outlook.com>
2024-05-25 18:17:05 -0400

    Added notations to all squares

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let screenHeight = window.innerHeight;
 let chessBoard = document.querySelector(".chessBoard");
+const notationLetters = ["a", "b", "c", "d", "e", "f", "g", "h"]
+const notationNumbers = [8, 7, 6, 5, 4, 3, 2, 1]
 
 if (!chessBoard) {
     console.error("No element with class 'chessBoard' found.");
@@ -9,17 +11,27 @@ if (!chessBoard) {
         let allSquares = "";
 
         // assigns a color to a square based on the color of the square in the previous column
-        function assignSquare(index) {
-            let color = (index % 2 === 0 && Math.ceil(index / 8) % 2 === 0) || (index % 2 !== 0 && Math.ceil(index / 8) % 2 !== 0) ? "tan" : "white";
-            let square = `<div class="${color}Square"> </div>`;
+        function assignSquare(row, column, notation) {
+            let color = (row % 2 === 0 && column % 2 === 0) || (row % 2 !== 0 && column % 2 !== 0) ? "white" : "tan";
+
+            let square = `<div class="${color}Square square" notation-value="${notation}"> </div>`;
             allSquares += square;
         }
 
         // the first color of the first row
         let color = "white";
 
-        for (let i = 1; i <= 64; i++) {
-           assignSquare(i);
+        for (let i = 0; i < 8; i++) {
+            let row = i;
+            let notationNumber = notationNumbers[i];
+            for (let j = 0; j < 8; j++) {
+                let column = j;  
+                let notationLetter = notationLetters[j];
+                let notation = notationLetter + notationNumber;
+                assignSquare(row, column, notation); 
+                console.log("notationLetter: ", notationLetter);
+           console.log("notationNumber: ", notationNumber);
+           }                     
         }
         chessBoard.innerHTML = allSquares;
     }
