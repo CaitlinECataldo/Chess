bb1528bc05868d0fae98216d9b73de83d73d1961
CaitlinECataldo <caitlinreitz@outlook.com>
2024-05-25 18:27:38 -0400

    Assigned starting positions to all chess pieces

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let screenHeight = window.innerHeight;
 let chessBoard = document.querySelector(".chessBoard");
 const notationLetters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 const notationNumbers = [8, 7, 6, 5, 4, 3, 2, 1]
+const startingPositions = {
+    white: {
+            king: "e1",
+            queen: "d1",
+            rook: ["a1", "h1"],
+            knight: ["b1", "g1"],
+            bishop: ["c1", "f1"],
+            pawn: ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
+    },
+    black: {
+        king: "e8",
+        queen: "d8",
+        rook: ["a8", "h8"],
+        knight: ["b8", "g8"],
+        bishop: ["c8", "f8"],
+        pawn: ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
+    }
+}
 
 if (!chessBoard) {
     console.error("No element with class 'chessBoard' found.");
