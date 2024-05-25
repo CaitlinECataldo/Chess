c8cdbad40bce68e5d83935097a375800f6928c4b
CaitlinECataldo <caitlinreitz@outlook.com>
2024-05-25 18:48:06 -0400

    Assigned images to chessmen object

--- script.js
+++ script.js
@@ -2,23 +2,55 @@ let screenHeight = window.innerHeight;
 let chessBoard = document.querySelector(".chessBoard");
 const notationLetters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 const notationNumbers = [8, 7, 6, 5, 4, 3, 2, 1]
-const startingPositions = {
+const chessmen = {
     white: {
-            king: "e1",
-            queen: "d1",
-            rook: ["a1", "h1"],
-            knight: ["b1", "g1"],
-            bishop: ["c1", "f1"],
-            pawn: ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
+            king: {
+                image: "/chess_pieces/white-king.png",
+                startingPosition: "e1"
+            },
+            queen: {
+                image: "/chess_pieces/white-queen.png",
+                startingPosition: "d1"},
+            rook: {
+                image: "/chess_pieces/white-rook.png",
+                startingPosition: ["a1", "h1"]
+                },
+            knight: {
+                image: "/chess_pieces/white-knight.png",
+                startingPosition: ["b1", "g1"]
+                },
+            bishop: {
+                image: "/chess_pieces/white-bishop.png",
+                startingPosition: ["c1", "f1"]
+                },
+            pawn: {
+                image: "/chess_pieces/white-pawn.png",
+                startingPosition: ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]}
     },
     black: {
-        king: "e8",
-        queen: "d8",
-        rook: ["a8", "h8"],
-        knight: ["b8", "g8"],
-        bishop: ["c8", "f8"],
-        pawn: ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
-    }
+        king: {
+            image: "/chess_pieces/black-king.png",
+            startingPosition: "e8"
+        },
+        queen: {
+            image: "/chess_pieces/black-queen.png",
+            startingPosition: "d8"},
+        rook: {
+            image: "/chess_pieces/black-rook.png",
+            startingPosition: ["a8", "h8"]
+            },
+        knight: {
+            image: "/chess_pieces/black-knight.png",
+            startingPosition: ["b8", "g8"]
+            },
+        bishop: {
+            image: "/chess_pieces/black-bishop.png",
+            startingPosition: ["c8", "f8"]
+            },
+        pawn: {
+            image: "/chess_pieces/black-pawn.png",
+            startingPosition: ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]}
+}
 }
 
 if (!chessBoard) {
@@ -32,7 +64,7 @@ if (!chessBoard) {
         function assignSquare(row, column, notation) {
             let color = (row % 2 === 0 && column % 2 === 0) || (row % 2 !== 0 && column % 2 !== 0) ? "white" : "tan";
 
-            let square = `<div class="${color}Square square" notation-value="${notation}"> </div>`;
+            let square = `<div class="${color}Square square" data-notation="${notation}"> </div>`;
             allSquares += square;
         }
 
@@ -47,11 +79,11 @@ if (!chessBoard) {
                 let notationLetter = notationLetters[j];
                 let notation = notationLetter + notationNumber;
                 assignSquare(row, column, notation); 
-                console.log("notationLetter: ", notationLetter);
-           console.log("notationNumber: ", notationNumber);
            }                     
         }
         chessBoard.innerHTML = allSquares;
     }
     createChessBoard();
+    let e1 = document.querySelector(`[data-notation="e1"]`);
+    e1.innerHTML = `<img class="chessmen" src="${chessmen.white.king.image}" >`;
 }
